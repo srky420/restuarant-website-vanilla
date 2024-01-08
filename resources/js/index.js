2c99be68ddb165c194da4ee3c418ccefb2bd0c3f
srky <srky420@gmail.com>
2024-01-08 21:15:06 +0500

    First commit

---
+++ resources/js/index.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', ()=> {
+    // Nav collapse
+    const navList = document.querySelector('.nav-list');
+    const burger = document.querySelector('.burger');
+
+    console.log(navList)
+    console.log(burger)
+
+    burger.addEventListener('click', (e) => {
+        navList.classList.toggle('nav-show');
+        burger.classList.toggle('burger-active')
+    })
+})
