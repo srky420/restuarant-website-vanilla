515c201532b52c97d2ab813cc31192e246068577
srky <srky420@gmail.com>
2024-01-11 14:59:02 +0500

    Add contact apge

--- resources/js/global.js
+++ resources/js/global.js
@@ -3,9 +3,6 @@ document.addEventListener('DOMContentLoaded', () => {
     const navList = document.querySelector('.nav-list');
     const burger = document.querySelector('.burger');
 
-    console.log(navList)
-    console.log(burger)
-
     burger.addEventListener('click', (e) => {
         navList.classList.toggle('nav-show');
         burger.classList.toggle('burger-active')
