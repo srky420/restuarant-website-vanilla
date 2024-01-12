ff52052be976c16059e502279e38e8e8ef133941
srky <srky420@gmail.com>
2024-01-12 12:21:50 +0500

    Add goto top btn

--- resources/js/global.js
+++ resources/js/global.js
@@ -1,11 +1,22 @@
-document.addEventListener('DOMContentLoaded', () => {
-    // Nav collapse
-    const navList = document.querySelector('.nav-list');
-    const burger = document.querySelector('.burger');
-
-    burger.addEventListener('click', (e) => {
-        navList.classList.toggle('nav-show');
-        burger.classList.toggle('burger-active')
+$('document').ready(() => {
+    // Nav collapse/show
+    $('.burger').click((e) => {
+        $('.nav-list').toggleClass('nav-show');
     })
 
+    // Goto top
+    $('#top-btn').click(() => window.scrollTo(0, 0));
+
 });
+
+// Show/hide goto top btn
+$(window).scroll(() => {
+    if ($(window).scrollTop() > $(document).height() / 3) {
+        $('.goto-top').addClass('show-btn');
+    }
+    else {
+        $('.goto-top').removeClass('show-btn');
+    }
+});
+
+ 
